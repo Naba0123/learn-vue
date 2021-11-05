b7915fa0df08f140a5715373750410f8a7d42935
Naba <11944066+Naba0123@users.noreply.github.com>
2021-11-06 08:50:53 +0900

    26_Composition_API_ライフサイクルフック

---
+++ 26_Composition_API_ライフサイクルフック.vue
@@ -0,0 +1,69 @@
+<script lang="js">
+
+// MyBook.vue
+
+export default {
+  setup() {
+    // mounted
+    onMounted(() => {
+      console.log('Component is mounted!')
+    })
+
+    // beforeMount
+    onBeforeMount(() => {
+      console.log('Component is beforeMount!')
+    })
+
+    // mounted
+    onMounted(() => {
+      console.log('Component is mounted!')
+    })
+
+    // beforeUpdate
+    onBeforeUpdate(() => {
+      console.log('Component is beforeUpdate!')
+    })
+
+    // updated
+    onUpdated(() => {
+      console.log('Component is updated!')
+    })
+
+    // beforeUnmount
+    onBeforeUnmount(() => {
+      console.log('Component is beforeUnmount!')
+    })
+
+    // unmounted
+    onUnmounted(() => {
+      console.log('Component is unmounted!')
+    })
+
+    // errorCaptured
+    onErrorCaptured(() => {
+      console.log('Component is errorCaptured!')
+    })
+
+    // renderTracked
+    onRenderTracked(() => {
+      console.log('Component is renderTracked!')
+    })
+
+    // renderTriggered
+    onRenderTriggered(() => {
+      console.log('Component is renderTrigger!')
+    })
+
+    // activated
+    onActivated(() => {
+      console.log('Component is activated!')
+    })
+
+    // deactivated
+    onDeactivated(() => {
+      console.log('Component is deactivated!')
+    })
+  }
+}
+
+</script>
