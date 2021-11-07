464de8fec714b9d6467368747fd15c8b54467d81
Naba <11944066+Naba0123@users.noreply.github.com>
2021-11-07 18:30:02 +0900

    38_単一ファイルコンポーネント

---
+++ 38_単一ファイルコンポーネント.vue
@@ -0,0 +1,43 @@
+<!--
+SFC
+https://v3.ja.vuejs.org/api/sfc-spec.html
+https://github.com/vuejs/vue-next/tree/master/packages/compiler-sfc
+Vite や VueCLI などのビルドツールを統合して、SFC を素早く使い始めることができる
+-->
+
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<template>
+  <p class="greeting">{{ greeting }}</p>
+</template>
+
+<style>
+.greeting {
+  color: red;
+  font-weight: bold;
+}
+</style>
+
+
+<!--
+
+使うとき
+
+import MyComponent from './MyComponent.vue'
+
+export default {
+  components: {
+    MyComponent
+  }
+}
+
+-->
